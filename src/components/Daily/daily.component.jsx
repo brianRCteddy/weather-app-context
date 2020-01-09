c13b9ad8d9af758d344ac9fc5a62900bea3e06d1
Teddy <brianrcdteodoro@gmail.com>
2020-01-09 08:19:24 +0800

    cleaned the code

--- src/components/Daily/daily.component.jsx
+++ src/components/Daily/daily.component.jsx
@@ -9,7 +9,7 @@ import DailyCard from './daily-card.component';
 const Daily = () => {
 	return (
 		<WeatherConsumer>
-			{({ dailyData, hourlyData }) => (
+			{({ dailyData }) => (
 				<div className="daily-forecast">
 					<h1 className="display-1 jumbotron">5 Day Forecast</h1>
 					<h5 className="display-5 text-muted">Mandaluyong, PH</h5>
--- src/components/Hourly/hourly.component.jsx
+++ src/components/Hourly/hourly.component.jsx
@@ -11,17 +11,15 @@ class Hourly extends Component {
 	state = {
 		loadedForecast: null
 	};
+
 	componentDidMount() {
 		const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=mandaluyong,ph&APPID=${apiConfig}`;
 
 		fetch(weatherUrl).then((res) => res.json()).then((data) => {
 			const forecastData = data.list.filter((reading) => reading.dt_txt.includes(this.props.match.params.day));
-			this.setState(
-				{
-					loadedForecast: forecastData
-				},
-				() => console.log(this.state)
-			);
+			this.setState({
+				loadedForecast: forecastData
+			});
 		});
 	}
 
@@ -34,17 +32,10 @@ class Hourly extends Component {
 		}
 		return (
 			<WeatherConsumer>
-				{({ hourlyData }) => (
+				{() => (
 					<div className="hourly-forecast">
-						<p>{this.props.match.params.dt_txt}</p>
 						<h1 className="display-1 jumbotron">Hourly Forecast</h1>
-						<div className="row justify-content-center">
-							{loadedForecast ? (
-								post
-							) : (
-								hourlyData.map((data, index) => <HourlyCard data={data} key={index} />)
-							)}
-						</div>
+						<div className="row justify-content-center">{post}</div>
 					</div>
 				)}
 			</WeatherConsumer>
@@ -52,25 +43,4 @@ class Hourly extends Component {
 	}
 }
 
-const LoadedCard = ({ data }) => {
-	let newDate = new Date(data.dt_txt);
-
-	const imgUrl = `owf owf-${data.weather[0].id} owf-5x`;
-	const celsius_min = data.main.temp_min - 273.15;
-	const celsius_max = data.main.temp_max - 273.15;
-	return (
-		<div className="card">
-			<h3 className="card-title">{moment(newDate).format('dddd')}</h3>
-			<p className="text-muted">{moment(newDate).format('MMMM Do, hh:mm a')}</p>
-			<i className={imgUrl} />
-			<h3>
-				<span className="min-temp">{Math.round(celsius_min)} °C -</span>
-				<span className="max-temp"> {Math.round(celsius_max)} °C</span>
-			</h3>
-			<div className="card-body">
-				<p className="card-text">{data.weather[0].description}</p>
-			</div>
-		</div>
-	);
-};
 export default withRouter(Hourly);
